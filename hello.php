65993222ca4957f6d0e168d226229e82274d612a
Faiyaz Rahman <faiyazrahman03@gmail.com>
2020-03-29 11:09:31 +0100

    Created hello.php and Created a Employee class

---
+++ hello.php
@@ -0,0 +1,39 @@
+<?php
+
+class Employee
+{
+    private $first_name;
+    private $last_name;
+    private $age;
+
+    public function __construct($first_name, $last_name, $age)
+    {
+        $this->first_name = $first_name;
+        $this->last_name = $last_name;
+        $this->age = $age;
+    }
+
+    public function getFirstName()
+    {
+        return $this->first_name;
+    }
+
+    public function getLastName()
+    {
+        return $this->last_name;
+    }
+
+    public function getAge()
+    {
+        return $this->age;
+    }
+}
+
+$employee = new Employee('Bob', 'Smith', 30);
+echo $employee->getFirstName();
+echo "<br/>";
+echo $employee->getLastName();
+echo "<br/>";
+echo $employee->getAge();
+
+
