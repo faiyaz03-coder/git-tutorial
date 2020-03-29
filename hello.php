c828e7e4361f25667ca99f44f76c022447a0d095
Faiyaz Rahman <faiyazrahman03@gmail.com>
2020-03-29 11:14:53 +0100

    Added a new method called getEmail

--- hello.php
+++ hello.php
@@ -5,12 +5,16 @@ class Employee
     private $first_name;
     private $last_name;
     private $age;
+    private $gender;
+    private $email;
 
-    public function __construct($first_name, $last_name, $age)
+    public function __construct($first_name, $last_name, $age, $gender, $email)
     {
         $this->first_name = $first_name;
         $this->last_name = $last_name;
         $this->age = $age;
+        $this->gender = $gender;
+        $this->email = $email;
     }
 
     public function getFirstName()
@@ -27,13 +31,27 @@ class Employee
     {
         return $this->age;
     }
+
+    public function getGender()
+    {
+        return $this->getGender();
+    }
+
+    public function getEmail()
+    {
+        return $this->getGender();
+    }
 }
 
-$employee = new Employee('Bob', 'Smith', 30);
+$employee = new Employee('Bob', 'Smith', 30, 'Male', 'faiyaz03@gmail.com');
 echo $employee->getFirstName();
 echo "<br/>";
 echo $employee->getLastName();
 echo "<br/>";
 echo $employee->getAge();
+echo "<br/>";
+echo $employee->getGender();
+echo "<br/>";
+echo $employee->getEmail();
 
 
